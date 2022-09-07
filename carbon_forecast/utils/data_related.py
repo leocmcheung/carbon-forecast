7b6d7cc65b7666cdf6bb2a6f8e0d28eb2ed6dd3a
Leo Cheung <leocmcheung@gmail.com>
2022-09-07 11:27:26 +0100

    Changing carbon-forecast to carbon_forecast, adding utils/data_related.py to help speed up data filtering and cleaning

---
+++ carbon_forecast/utils/data_related.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+
+
+
+
+
+def show_sector():
+    """
+    To help with initial data filtering on Historical Emission csv
+    """
+    sector = input("Which GICS Sector?")
+    print("Reading data...")
+    data = pd.read_csv("../raw_data/Historical_Emissions_simp.csv")
+    print("Getting rid of 2021 incomplete data...")
+    data = data[data["Financial Year"] < 2021]
+    print("Adding scope1&2 -> For absolute please look 'C-absolute-1+2'\n For intensity please look 'C-intensity-1+2'")
+    data["C-absolute-1+2"] = data["Carbon-Scope 1 (tonnes CO2e)"] + data["Carbon-Scope 2 (tonnes CO2e)"]
+    data["C-intensity-1+2"] = data["Carbon Intensity-Scope 1 (tonnes CO2e/USD mn)"] + data["Carbon Intensity-Scope 2 (tonnes CO2e/USD mn)"]
+    print("Filtering by GICS Industry Name")
+    data_filtered = data[data["GICS Sector Name"] == f"{sector}"]
+    print("Done!")
+    return data_filtered
+
+
+if __name__ == '__main__':
+    show_sector()
